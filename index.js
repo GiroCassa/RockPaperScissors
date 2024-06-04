e5fe59f2eb6817b25cc80172d739cd3a391ca1c5
GiroCassa <girocassa@DESKTOP-DUQ7BO8>
2024-06-04 12:47:23 +0200

    added the playRound function for confronting the player and the humanchoice and playGame function for playing 5 round plus final debugging

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let human_score = 0;
+let computer_score = 0;
+
+playGame();
+
 function getComputerChoice() {
     let random_computer = Math.floor((Math.random() * 3) + 1);
     let computer_choice;
@@ -12,12 +17,12 @@ function getComputerChoice() {
             computer_choice = "scissors";
             break;
     }
-    return computer_choice;pap
+    return computer_choice;
 }
 
 function getHumanChoice () {
     while (true) {
-        let human_choice = window.prompt("Select an Option (rock/paper/scissors)");
+        let human_choice = (window.prompt("Select an Option (rock/paper/scissors)")).toLowerCase();
         if (human_choice == "rock") {
             return human_choice;
         } else if (human_choice == "paper") {
@@ -30,5 +35,59 @@ function getHumanChoice () {
     }
 }
 
-const human_choice = getHumanChoice();
-const computer_choice = getComputerChoice();
+function playRound (human, computer) {
+    if (human == "rock") {
+        switch (computer) {
+            case "rock":
+                alert(`The computer choose ${computer}, therefore it's a tie!`);
+                break;
+            case "paper":
+                alert(`The computer choose ${computer}, therefore you lose!`);
+                computer_score += 1;
+                break;
+            case "scissors":
+                alert(`The computer choose ${computer}, therefore you win!`);
+                human_score += 1;
+                break;
+        }
+    } else if (human == "paper") {
+        switch (computer) {
+            case "rock":
+                alert(`The computer choose ${computer}, therefore you win!`);
+                human_score += 1;
+                break;
+            case "paper":
+                alert(`The computer choose ${computer}, therefore it's a tie!`);
+                break;
+            case "scissors":
+                alert(`The computer choose ${computer}, therefore you lose!`);
+                computer_score += 1;
+                break;
+        }
+    } else {
+        switch (computer) {
+            case "rock":
+                alert(`The computer choose ${computer}, therefore you lose!`);
+                computer_score += 1;
+                break;
+            case "paper":
+                alert(`The computer choose ${computer}, therefore you win!`);
+                human_score += 1;
+                break;
+            case "scissors":
+                alert(`The computer choose ${computer}, therefore it's a tie!`);
+                break;
+        }
+    }
+}
+
+function playGame () {
+    for (let i = 0; i < 5; i++) {
+        const human_choice = getHumanChoice();
+        const computer_choice = getComputerChoice();
+        playRound(human_choice, computer_choice);
+    }
+    human_score > computer_score ? alert(`You Win! Your score is ${human_score}, the computer score is ${computer_score}.`) :
+    human_score === computer_score ? alert(`It's a Tie! You and the computer have both a score of ${human_score}`):
+    alert(`You Lose! Your score is ${human_score}, the computer score is ${computer_score}.`);
+}
