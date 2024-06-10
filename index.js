3e6797eb49fe820db57cb8971f309cfde3b394fa
GiroCassa <girocassa@DESKTOP-DUQ7BO8>
2024-06-10 21:09:48 +0200

    added custom function to play any amount of round plus some basic responsive styling plus edit to the readme file

--- index.js
+++ index.js
@@ -5,26 +5,33 @@ const playerScore = document.querySelector("#playerscr");
 const computerScore = document.querySelector("#computerscr");
 const computerText = document.querySelector("#computerText");
 
+const restartBtn = document.createElement("button");
+restartBtn.id = "restartbtn";
+restartBtn.textContent = "Play Again?";
+restartBtn.setAttribute("style", "margin-top: 20px; font-size: 38px; font-weight: 700; background-color: #f97e71; width: 250px; height: 50px");
+
 let human_score = 0;
 let computer_score = 0;
-let roundPlayed = 0;
+let roundStart = 0;
+let roundToFinish = askRounds();
 
-playerScore.textContent = `Player Score: ${human_score}`;
-computerScore.textContent = `Computer Score: ${computer_score}`;
-computerText.textContent = "the computer choice will be shown here."
-
-rockBtn.addEventListener("click", rockChoice)
-scissorsBtn.addEventListener("click", scissorsChoice)
-paperBtn.addEventListener("click", paperChoice)
+rockBtn.addEventListener("click", rockChoice);
+scissorsBtn.addEventListener("click", scissorsChoice);
+paperBtn.addEventListener("click", paperChoice);
+playerScore.innerText = `Player Score: ${human_score}`;
+computerScore.innerText = `Computer Score: ${computer_score}`;
+computerText.textContent = "the computer choice will be shown here.";
 
 function rockChoice () {
     let human_choice = "rock";
     startGame(human_choice);
 }
+
 function paperChoice () {
     let human_choice = "paper";
     startGame(human_choice);
 }
+
 function scissorsChoice () {
     let human_choice = "scissors";
     startGame(human_choice);
@@ -95,21 +102,20 @@ function playRound (human, computer) {
                 computerText.textContent = `The computer choose ${computer}, therefore it's a tie!`;
                 break;
         }   
-    }  playerScore.textContent = `Player Score: ${human_score}`;
-       computerScore.textContent = `Computer Score: ${computer_score}`;
-       roundPlayed+= 1;
-       console.log(roundPlayed);
-       checkEnd(roundPlayed);
+    }  playerScore.innerText = `Player Score: ${human_score}`;
+       computerScore.innerText = `Computer Score: ${computer_score}`;
+       roundStart += 1;
+       checkEnd(roundStart);
 }
 
 function checkEnd(rounds) {
-    if (rounds == 5) results();
+    if (rounds == roundToFinish) results();
 }
 
 function results() {
     human_score > computer_score ? computerText.innerHTML = computerText.textContent + "<br>You Won The Game!":
-    human_score === computer_score ? computerText.innerHTML = computerText.textContent + "<br>You Lost The Game!":
-    computerText.innerHTML = computerText.textContent + "<br>The Game Ends With a Draw!";
+    human_score === computer_score ? computerText.innerHTML = computerText.textContent + "<br>The Game Ends With a Draw!":
+    computerText.innerHTML = computerText.textContent + "<br>You Lost The Game!";
     endGame()
 }
 
@@ -117,4 +123,26 @@ function endGame() {
     rockBtn.removeEventListener("click", rockChoice);
     paperBtn.removeEventListener("click", paperChoice);
     scissorsBtn.removeEventListener("click", scissorsChoice);
+    document.querySelector(".text").appendChild(restartBtn);
+    restartBtn.addEventListener("click", restartGame);
+}
+
+function restartGame() {
+    human_score = 0;
+    computer_score = 0;
+    roundStart = 0
+    rockBtn.addEventListener("click", rockChoice);
+    scissorsBtn.addEventListener("click", scissorsChoice);
+    paperBtn.addEventListener("click", paperChoice);
+    playerScore.innerText = `Player Score: ${human_score}`;
+    computerScore.innerText = `Computer Score: ${computer_score}`;
+    computerText.textContent = "the computer choice will be shown here.";
+    restartBtn.removeEventListener("click", restartGame);
+    document.querySelector(".text").removeChild(restartBtn);
+    roundToFinish = askRounds();
+}
+
+function askRounds() {
+    let roundToPlay = Number((window.prompt("How many rounds would you like to play? (0 for infinite mode)")));
+    return roundToPlay;
 }
